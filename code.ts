321b85d08af846ce72693cb5892977e5a22cf995
HellSquirrel <zloymult@gmail.com>
2020-07-27 18:25:10 +0300

    Add linter rules

--- code.ts
+++ code.ts
@@ -1,13 +1,11 @@
 // Helpers
 
-
 function getStyleNameFromNode(node) {
-  if (!node || !('fillStyleId' in node) || (node.fillStyleId === '')) return;
+  if (!node || !("fillStyleId" in node) || node.fillStyleId === "") return;
   const styleId = node.fillStyleId;
   const style = figma.getStyleById(styleId);
   return style?.name;
 }
-
 
 const themeHelper = (() => {
   const regexThemeSlash = /theme.+?(?=\/|\s+\/)/i;
@@ -25,21 +23,18 @@ const themeHelper = (() => {
 
   return {
     getThemeNameFromString,
-    replaceThemeInName
-  }
+    replaceThemeInName,
+  };
 })();
-
-
 
 // Handle themes
 
-
 function getThemesNames() {
   const paintStyles = figma.getLocalPaintStyles();
-  const paintStylesNames = paintStyles.map(style => style.name);
+  const paintStylesNames = paintStyles.map((style) => style.name);
 
-  let themesNames = [];
-  paintStylesNames.forEach(styleName => {
+  const themesNames = [];
+  paintStylesNames.forEach((styleName) => {
     const themeName = themeHelper.getThemeNameFromString(styleName);
     if (themeName) themesNames.push(themeName);
   });
@@ -49,128 +44,115 @@ function getThemesNames() {
   return themesNamesUnique;
 }
 
-
 function handleThemes() {
   const themesNames = getThemesNames();
-  const data = themesNames.map(themeName => ({
+  const data = themesNames.map((themeName) => ({
     themeName,
-    cssId: getCSSIdForThemeName(themeName)
+    cssId: getCSSIdForThemeName(themeName),
   }));
   figma.ui.postMessage({
-    type: 'render',
-    data
+    type: "render",
+    data,
   });
 }
 
-
-
 // Handle selection
 
-
 function getCSSIdForThemeName(themeName: string): string {
-  return themeName.replace(/\s+/g, '-').toLowerCase();
+  return themeName.replace(/\s+/g, "-").toLowerCase();
 }
 
-
 function getThemeNameFromNode(node) {
-  if (!node || !('fillStyleId' in node) || (node.fillStyleId === '')) return null;
+  if (!node || !("fillStyleId" in node) || node.fillStyleId === "") return null;
   const styleName = getStyleNameFromNode(node);
   const themeName = themeHelper.getThemeNameFromString(styleName);
   if (themeName) return themeName;
   return null;
 }
 
-
 function handleSelection() {
   const [firstNode] = figma.currentPage.selection;
   if (!firstNode) {
-    figma.ui.postMessage({ type: 'options-disable-all' });
+    figma.ui.postMessage({ type: "options-disable-all" });
     return;
   }
   let themeName = getThemeNameFromNode(firstNode);
   if (themeName) {
-    figma.ui.postMessage({ type: 'options-enable-all' });
+    figma.ui.postMessage({ type: "options-enable-all" });
     const cssId = getCSSIdForThemeName(themeName);
     figma.ui.postMessage({
-      type: 'options-select-theme',
-      cssId
+      type: "options-select-theme",
+      cssId,
     });
     return;
   }
-  if (!('findOne' in firstNode)) {
-    figma.ui.postMessage({ type: 'options-disable-all' });
+  if (!("findOne" in firstNode)) {
+    figma.ui.postMessage({ type: "options-disable-all" });
     return;
   }
   themeName = getThemeNameFromNode(
-    firstNode?.findOne(node => !!getThemeNameFromNode(node))
+    firstNode?.findOne((node) => !!getThemeNameFromNode(node))
   );
   if (themeName) {
-    figma.ui.postMessage({ type: 'options-enable-all' });
+    figma.ui.postMessage({ type: "options-enable-all" });
     const cssId = getCSSIdForThemeName(themeName);
     figma.ui.postMessage({
-      type: 'options-select-theme',
-      cssId
+      type: "options-select-theme",
+      cssId,
     });
     return;
   }
-  figma.ui.postMessage({ type: 'options-disable-all' });
+  figma.ui.postMessage({ type: "options-disable-all" });
 }
-
-
 
 // Switch theme for selection
 
-
 function getStyleByName(name) {
   const styles = figma.getLocalPaintStyles();
-  const styleWithGivenName = styles.find(style => style.name === name);
+  const styleWithGivenName = styles.find((style) => style.name === name);
   return styleWithGivenName;
 }
 
-
 function switchThemeForNode(node, targetThemeName) {
   const styleName = getStyleNameFromNode(node);
-  const styleNameWithTargetTheme = themeHelper.replaceThemeInName(styleName, targetThemeName);
+  const styleNameWithTargetTheme = themeHelper.replaceThemeInName(
+    styleName,
+    targetThemeName
+  );
   const styleWithTargetTheme = getStyleByName(styleNameWithTargetTheme);
   node.fillStyleId = styleWithTargetTheme?.id;
 }
 
-
-
 // Start point
-
 
 figma.showUI(__html__);
 
 handleThemes();
 handleSelection();
 
-
-
 // Events handlers
 
-
-figma.ui.onmessage = msg => {
-
-  switch(msg.type) {
-
-    case 'switch-theme-for-selected-layout': {
+figma.ui.onmessage = (msg) => {
+  switch (msg.type) {
+    case "switch-theme-for-selected-layout": {
       const [firstNode] = figma.currentPage.selection;
       if (!firstNode) break;
       let nodesWithTheme = [];
-      if ('findAll' in firstNode) {
-        nodesWithTheme = firstNode.findAll(node => !!getThemeNameFromNode(node));
+      if ("findAll" in firstNode) {
+        nodesWithTheme = firstNode.findAll(
+          (node) => !!getThemeNameFromNode(node)
+        );
       }
-      if (!!getThemeNameFromNode(firstNode)) nodesWithTheme.push(firstNode);
-      nodesWithTheme.forEach(node => switchThemeForNode(node, msg.themeName));
+      if (getThemeNameFromNode(firstNode)) nodesWithTheme.push(firstNode);
+      nodesWithTheme.forEach((node) => switchThemeForNode(node, msg.themeName));
 
       break;
     }
 
-    default: { break; }
-
+    default: {
+      break;
+    }
   }
 };
-
 
 figma.on("selectionchange", handleSelection);
